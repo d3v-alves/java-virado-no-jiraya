58a9d80c4e04b6c907168caa26d2ed3add35184a
Douglas <douglas.v849@gmail.com>
2026-09-24 18:41:34 -0300

    Continuation

---
+++ src/academy/devdojo/maratonajava/javacore/execption/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.execption.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked and Unchecked
+        //Object object =  null;
+        int[] nums = {1, 2};
+        System.out.println(nums[3]);
+    }
+}
